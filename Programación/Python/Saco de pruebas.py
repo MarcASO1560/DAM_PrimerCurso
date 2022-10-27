7d3995007b00ee14a3422486ba71d5da7c04a325
Marc Albert Seguí Olmos <maraka@pop-os.localdomain>
2022-10-27 08:24:18 +0200

    Si.

--- Programación/Python/Saco de pruebas.py
+++ Programación/Python/Saco de pruebas.py
@@ -1,18 +1,2 @@
-#Actividad 26:
-print("Actividad 26:")
-while True:
-    min = float(input("Introduce un número:"))
-    max = float(input("Introduce un número anterior que el anterior:"))
-    y = []
-    if min > max:
-        print(min,"es mayor que",max,"Por lo tanto vuelve a empezar.")
-    else:
-        x = float(input("Elige un número que esté entre el minimo y el maximo elegidos:"))
-        while True:
-            if x >= min and x <= max:
-                y.append(x)
-                x = float(input("Elige otro número que esté entre el minimo y el maximo elegidos:"))
-            else:
-                break
-        print("Los números situados entre",min,"y",max,"son:",y)
-        break
+#Actividad 32:
+print("Actividad 32:")
