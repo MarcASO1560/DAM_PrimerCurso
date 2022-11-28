a1a276441e3f483850f6dc77e56308a7e6aa953e
MaraKa.exe <94853428+MarcASO1560@users.noreply.github.com>
2022-11-28 02:41:51 +0100

    Update Actividades2.py

--- Programación/Python/Actividades2.py
+++ Programación/Python/Actividades2.py
@@ -149,4 +149,71 @@ os.system ("clear")
 print("Actividad 58")
 #En consola.
 #----------------------------------------------------------------------------------------------------------------------------------
-
+#Actividad 59.
+os.system ("clear")
+print("Actividad 59")
+x = int(input("Cuantos datos quieres ingresar? "))
+y = []
+num = 0
+for i in range(1,x+1):
+    i = float(input(f"Dato {i}:"))
+    y.append(i)
+    num += i
+mid = num/x
+c = 0
+for i in y:
+    if i > mid:
+        c += 1
+if c == 1:
+    print(c,"dato es mayor que el promedio.")
+elif c > 1:
+    print(c,"datos son mayores que el promedio.")
+#----------------------------------------------------------------------------------------------------------------------------------
+#Actividad 60.
+os.system ("clear")
+print("Actividad 60")
+def contar_letras(x):
+    d = {}
+    x = x.lower()
+    for i in x:
+        if i.isalnum() == True:
+            if i in d:
+                d[i] += 1
+            else:
+                d[i] = 1
+    print(d)
+x = input("Escribe una oración: ")
+contar_letras(x)
+#----------------------------------------------------------------------------------------------------------------------------------
+#Actividad 61.
+os.system ("clear")
+print("Actividad 61")
+def contar_iniciales(x):
+    d = {}
+    x = x.lower()
+    x = x.split()
+    for i in x:
+        i = i[0]
+        if i.isalnum() == True:
+            if i in d:
+                d[i] += 1
+            else:
+                d[i] = 1
+    print(d)
+x = input("Escribe una oración: ")
+contar_iniciales(x)
+#----------------------------------------------------------------------------------------------------------------------------------
+#Actividad 62.
+os.system ("clear")
+print("Actividad 62")
+def contar_largo_palabras(x):
+    d = {}
+    x = x.lower()
+    x = x.split()
+    for i in x:
+        num = len(i)
+        if i.isalnum() == True:
+            d[i] = num
+    print(d)
+x = input("Escribe una oración: ")
+contar_largo_palabras(x)
